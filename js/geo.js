33e38067bad9aea1b6dd7dcf407b6f7478e454a8
Murat Knecht <murat.knecht@gmx.de>
2014-08-10 19:02:12 +1200

    Tested and implemented conversion of WGS84 coordinates to image coordinates suitable for css.

--- js/geo.js
+++ js/geo.js
@@ -1,14 +1,57 @@
 ;(function (root) {
-  function EqRectMapping(nw, se) {
-    this.northwest = nw
-    this.southeast = se
+  var geomod = module.exports
+  var GRAD = 60 * 60
+  function LatLonSecs(lat, lon) {
+    this.latSecs = lat
+    this.lonSecs = lon
+
+    this.minus = function(other) {
+      return new LatLonSecs(
+        this.latSecs - other.latSecs,
+        this.lonSecs - other.lonSecs
+      )
+    }
   }
 
-  module.exports.createMapping = function(nw, se, width, height) {
-    new EqRectMapping(
-      [nw, se],
-      [width, height]
+  function toSeconds(latLon) {
+    function toSec(component) {
+      return Math.round(component * GRAD)
+    }
+
+    return new LatLonSecs(
+      toSec(latLon._lat),
+      toSec(latLon._lon)
     )
-    return [0,0]
+  }
+
+  function simplifyBase(latLonSecs) {
+    return new LatLonSecs(
+      latLonSecs.latSecs + (90 * GRAD),
+      latLonSecs.lonSecs + (180 * GRAD)
+    )
+  }
+  
+  function EqRectMapping(ne, sw, width, height) {
+    var baseSecs = simplifyBase(toSeconds(sw))
+    var diffSecs = simplifyBase(toSeconds(ne)).minus(baseSecs)
+
+    this.worldToLocal = function(world) {
+      var worldDiffSecs = simplifyBase(toSeconds(world)).minus(baseSecs)
+      return [
+        width * (worldDiffSecs.lonSecs / diffSecs.lonSecs),
+        height - height * (worldDiffSecs.latSecs / diffSecs.latSecs)
+      ].map(Math.round)
+    }
+  }
+
+  function createMapping(nw, se, width, height) {
+    return new EqRectMapping(nw, se, width, height)
+  }
+  
+  module.exports = {
+    createMapping: createMapping,
+    LatLonSecs: LatLonSecs,
+    simplifyBase: simplifyBase,
+    toSeconds: toSeconds
   }
 }(this))
--- tests/geo-spec.js
+++ tests/geo-spec.js
@@ -9,20 +9,79 @@ var se = new LatLon(-48.3, 179.4)
 
 var should = require('should')
 
+GRAD = 1
+MIN = GRAD / 60
+SEC = MIN / 60
+
+describe("toSeconds", function() {
+  it("handle basics (0, 0)", function() {
+    var latLonSec = learngeo.toSeconds(new LatLon(0, 0))
+    latLonSec.latSecs.should.equal(0)
+    latLonSec.lonSecs.should.equal(0)
+  });
+  it("handle basics (1 degree, 1 degree)", function() {
+    var latLonSec = learngeo.toSeconds(new LatLon(1 * GRAD, 1 * GRAD))
+    latLonSec.latSecs.should.equal(1 * 60 * 60)
+    latLonSec.lonSecs.should.equal(1 * 60 * 60)
+  });
+  it("combined: 1 degree 1 min, 1 degree 1 min", function() {
+    var latLonSec = learngeo.toSeconds(new LatLon(1 * GRAD + 2 * MIN, 1 * GRAD + 2 * MIN))
+    latLonSec.latSecs.should.equal(1 * 60 * 60 + 2 * 60)
+    latLonSec.lonSecs.should.equal(1 * 60 * 60 + 2 * 60)
+  });
+  it("combined: 1 degree 1 sec, 1 degree 1 sec", function() {
+    var latLonSec = learngeo.toSeconds(new LatLon(1 * GRAD + 2 * SEC, 1 * GRAD + 2 * SEC))
+    latLonSec.latSecs.should.equal(1 * 60 * 60 + 2)
+    latLonSec.lonSecs.should.equal(1 * 60 * 60 + 2)
+  });
+})
+
+describe("Simplify coordinates by making them positive", function() {
+  var simplify = learngeo.simplifyBase
+  var LatLonSecs = learngeo.LatLonSecs
+  
+  it("should make max negative coordinate positive", function() {
+    var simple = simplify(new LatLonSecs(-90 * 60 * 60, -180 * 60 * 60))
+    simple.latSecs.should.not.be.below(0)
+    simple.lonSecs.should.not.be.below(0)
+  })
+  
+  it("should keep difference", function() {
+    var first = 60
+    var second = -20
+    var firstSimple = simplify(new LatLonSecs(first, first * 60))
+    var secondSimple = simplify(new LatLonSecs(second, second * 60))
+    ;(secondSimple.latSecs - firstSimple.latSecs).should.be.equal(second - first)
+    ;(secondSimple.lonSecs - firstSimple.lonSecs).should.be.equal(second * 60 - first * 60)
+  })
+})
+
 describe("Coordinate mapping module: 0,0 -> 50,50", function() {
-  var se = new LatLon(0, 50)
-  var nw = new LatLon(50, 0)
+  var north = 50 * GRAD
+  var west = 0 * GRAD
+  var south = 0 * GRAD
+  var east = 50 * GRAD
+  var sw = new LatLon(south, west)
+  var ne = new LatLon(north, east)
   var height = 100
   var width = 100
+  
   it("Factory is defined", function() {
-    learngeo.createMapping(nw, se, width, height)
+    learngeo.createMapping(ne, sw, width, height)
     .should.be.ok
   });
   
   it("Northwest corner is (0,0)", function() {
     should.deepEqual(
-      learngeo.createMapping(nw, se, width, height),
+      learngeo.createMapping(ne, sw, width, height).worldToLocal(new LatLon(north, west)),
       [0, 0]
+    )
+  });
+  
+  it("Southwest corner is (0,[image height])", function() {
+    should.deepEqual(
+      learngeo.createMapping(ne, sw, width, height).worldToLocal(new LatLon(south, west)),
+      [0, height]
     )
   });
 });
