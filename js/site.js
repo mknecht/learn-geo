59c8abf877f57a130d051acc453a078da10f8c07
Murat Knecht <murat.knecht@gmx.de>
2014-08-10 14:17:39 +1200

    js3 code formatting~

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 /*global jQuery, Snap */
 
-"use strict";
-
 (function(w, $, Snap) {
-    var $w = $(window);
-    $(function() {
-	$('img#nz-map').width($w.width());
-        var svg = $('<svg id="cursor-shadow"><rect/></svg>').appendTo('body').css({height: 25, width: 25});
-	var s = Snap('svg#cursor-shadow');
-	var c = s.circle(svg.width()/2, svg.height()/2, Math.min(svg.width(), svg.height())/2 - 2);
-	$(window).mousemove(function(e) {
-	    svg.css({left: e.pageX - svg.width()/2, top:e.pageY - svg.height()/2});
-	});
+  "use strict";
+  var $w = $(window);
+  $(function() {
+    $('img#nz-map').width($w.width());
+    var svg = $('<svg id="cursor-shadow"><rect/></svg>').appendTo('body').css({height: 25, width: 25});
+    /*jshint newcap:false */
+    var s = Snap('svg#cursor-shadow');
+    var c = s.circle(svg.width()/2, svg.height()/2, Math.min(svg.width(), svg.height())/2 - 2);
+    $(window).mousemove(function(e) {
+      svg.css({left: e.pageX - svg.width()/2, top:e.pageY - svg.height()/2});
     });
+  });
 })(window, jQuery, Snap);
