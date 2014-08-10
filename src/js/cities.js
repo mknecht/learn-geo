8df61084c9480f80c532bf8caf09e841ecdab6c6
Murat Knecht <murat.knecht@gmx.de>
2014-08-11 00:36:26 +1200

    Added Auckland and Wellington on the map.

--- src/js/cities.js
+++ src/js/cities.js
@@ -1,6 +1,6 @@
 /*global jQuery, Snap */
 
-(function(w, $, d3) {
+(function(w, $, d3, LatLon, learngeo) {
   "use strict";
   var $w = $(window);
   $(function() {
@@ -22,5 +22,41 @@
       .style('left', Math.round(e.pageX - $svg.width()/2) + 'px')
       .style('top', Math.round(e.pageY - $svg.height()/2) + 'px');
     });
-  });
-})(window, require('jquery'), require('d3'));
+
+    var cities = [
+      {name: "Auckland", loc: new LatLon(-36.840417, 174.739869), size: 30},
+      {name: "Wellington", loc: new LatLon(-41.288889, 174.777222), size: 10}
+    ]
+    
+    var north = -34
+    var west = 165.8
+    var south = -48.3
+    var east = 179.4
+    var sw = new LatLon(south, west)
+    var ne = new LatLon(north, east)
+    var height = $('img#nz-map').height()
+    var width = $('img#nz-map').width()
+    var mapping = learngeo.createMapping(ne, sw, width, height)
+    
+    d3.select('body')
+    .append('svg')
+    .attr('height', height)
+    .attr('width', width)
+    .style({position:'absolute', top: '0px', left: '0px'})
+    .append('g')
+    .selectAll('circle')
+    .data(cities)
+    .enter()
+    .append('circle')
+    .attr('class', 'city')
+    .attr('r', function(d) { return d.size + 'px' })
+    .attr('cx', function (d) { return mapping.worldToLocal(d.loc)[0] + 'px'})
+    .attr('cy', function (d) { return mapping.worldToLocal(d.loc)[1] + 'px'})
+                                                                         
+  })
+})(window,
+   require('jquery'),
+   require('d3'),
+   require('mt-latlon'),
+   require('./geo')
+  )
