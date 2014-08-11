016e9c1d432680c26962fe76f7daebdb170ff8cd
Murat Knecht <murat.knecht@gmx.de>
2014-08-11 22:19:15 +1200

    Rebuilt the SVG with Angular. Sigh.

--- src/js/cities.js
+++ src/js/cities.js
@@ -3,79 +3,14 @@
 (function(w, $, d3, angular, LatLon, learngeo) {
   "use strict";
   var $w = $(window);
+  
   $(function() {
-    function setupMouse() {
-      console.log("setting up mouse!")
-      var svg = d3.select('body')
-                .append('svg')
-                .attr('id', 'cursor-shadow')
-                .attr('height', 25)
-                .attr('width', 25)
-      var $svg = $('svg#cursor-shadow')
-      /*jshint newcap:false */
-      var circle = svg
-                   .append('circle')
-                   .attr('cx', $svg.width()/2)
-                   .attr('cy', $svg.height()/2)
-                   .attr('r', Math.min($svg.width(), $svg.height())/2 - 2)
-      $(window).mousemove(function(e) {
-        svg
-        .style('left', Math.round(e.pageX - $svg.width()/2) + 'px')
-        .style('top', Math.round(e.pageY - $svg.height()/2) + 'px');
-      });
-    }
-    
-    function setupMap() {
-      var images = [{'id':'nz-map', src:'../images/nz.jpg'}]
-
-      var cities = [
-        {name: "Auckland", loc: new LatLon(-36.840417, 174.739869), size: 30},
-        {name: "Wellington", loc: new LatLon(-41.288889, 174.777222), size: 10}
-      ]
-      
-      var north = -34
-      var west = 165.8
-      var south = -48.3
-      var east = 179.4
-      var sw = new LatLon(south, west)
-      var ne = new LatLon(north, east)
-      var svg = d3.select('body')
-                .append('svg')
-                .attr({
-                  'xmlns': 'http://www.w3.org/2000/svg',
-                  'xmlns:xmlns:xlink': 'http://www.w3.org/1999/xlink'
-                })
-
-      svg.selectAll('image')
-      .data(images)
-      .enter()
-      .append('svg:image')
-      .attr('id', function(d) { return d.id })
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', $w.width() + 'px')
-      .attr('height', '1200px')
-      .attr('xlink:href', function(d) { return d.src })
-
-      var $img = $('image')
-      var mapping = learngeo.createMapping(ne, sw, $img.width(), $img.height())
-      
-
-      svg.append('g')
-      .selectAll('circle')
-      .data(cities)
-      .enter()
-      .append('circle')
-      .attr('class', 'city')
-      .attr('r', function(d) { return d.size + 'px' })
-      .attr('cx', function (d) { return mapping.worldToLocal(d.loc)[0] + 'px'})
-      .attr('cy', function (d) { return mapping.worldToLocal(d.loc)[1] + 'px'})
-    }
-
     var app = angular.module('citiesApp', [])
     app.factory('globalState', function () {
       return {
-        level: 'menu'
+        height: 1700,
+        level: window.location.hash.substring(1),
+        width: $('body').innerWidth()
       }
     })
     
@@ -90,21 +25,26 @@
       '$scope', 'globalState',
       function($scope, globalState) {
         var levels = {
-          menu: function() {
+          easy: {
+            layers: ['map', 'selection']
           },
-          solution: function() {
-            setupMap()
-            setupMouse()
+          menu: {
+            layers: ['menu']
+          },
+          solution: {
+            layers: ['map', 'solution']
           }
         }
-        
-        $scope.currentLevel = globalState.level
-        $scope.globalState = globalState
-        $scope.$watch('globalState.level', function() {
+
+        function updateScope() {
           $scope.currentLevel = globalState.level
-          levels[globalState.level]()
-        })
+          $scope.layers = levels[globalState.level].layers
+        }
+        updateScope()
+        $scope.globalState = globalState
+        $scope.$watch('globalState.level', updateScope)
       }])
+    
     app.controller('MenuCtrl', [
       '$scope', 'globalState', 'menuItems',
       function($scope, globalState, menuItems) {
@@ -113,6 +53,77 @@
           globalState.level = item.selectsLevel
         }
       }])
+    
+    app.controller('SvgCtrl', [
+      "$scope",
+      "globalState",
+      "$element",
+      "$document",
+      function ($scope, globalState, $element, $document) {
+        $scope.width = globalState.width
+        $scope.height = globalState.height
+        globalState.mouse = {x: 200, y:200}
+        $element.bind("mousemove", function(e) {
+          globalState.mouse.x = e.pageX
+          globalState.mouse.y = e.pageY
+          $scope.$apply()
+        })
+      }        
+    ])
+    
+    app.controller('TopoMapCtrl', [
+      "$scope",
+      "globalState",
+      function ($scope, globalState) {
+        $scope.id = 'nz-map'
+        $scope.width = globalState.width
+        $scope.height = globalState.height
+      }        
+    ])
+
+    app.factory('cities', function() {
+      return [
+        {name: "Auckland", loc: new LatLon(-36.840417, 174.739869), size: 30},
+        {name: "Wellington", loc: new LatLon(-41.288889, 174.777222), size: 10}
+      ]
+    })
+
+    app.controller('SolutionCtrl', [
+      '$scope', 'cities', 'globalState',
+      function($scope, cities, globalState) {
+        var north = -34
+        var west = 165.8
+        var south = -48.3
+        var east = 179.4
+        var sw = new LatLon(south, west)
+        var ne = new LatLon(north, east)
+        $scope.cities = cities.map(
+          function(city) {
+            var mapping = learngeo.createMapping(ne, sw, globalState.width, globalState.height)
+            var local = mapping.worldToLocal(city.loc)
+            return {
+              cx: local[0],
+              cy: local[1],
+              r: city.size
+            }
+          }
+        )
+      }])
+
+    app.controller('SelectCtrl', [
+      '$scope',
+      'globalState',
+      function($scope, globalState) {
+        $scope.r = 20
+        $scope.$watch(
+          function() {
+            return globalState.mouse
+          },
+          function(newpos) {
+            $scope.cx = newpos.x
+            $scope.cy = newpos.y
+        }, true)
+    }])
   })
   
 })(window,
