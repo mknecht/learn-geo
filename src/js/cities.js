a14ba3a6c304f3d78103c2bd64b116d1d45d8481
Murat Knecht <murat.knecht@gmx.de>
2014-08-11 12:06:48 +1200

    Image is now included via d3 into the svg element, in preparation for different levels with different images

--- src/js/cities.js
+++ src/js/cities.js
@@ -4,56 +4,77 @@
   "use strict";
   var $w = $(window);
   $(function() {
-    $('img#nz-map').width($w.width());
-    var svg = d3.select('body')
-              .append('svg')
-              .attr('id', 'cursor-shadow')
-              .attr('height', 25)
-              .attr('width', 25)
-    var $svg = $('svg#cursor-shadow')
-    /*jshint newcap:false */
-    var circle = svg
-                 .append('circle')
-                 .attr('cx', $svg.width()/2)
-                 .attr('cy', $svg.height()/2)
-                 .attr('r', Math.min($svg.width(), $svg.height())/2 - 2)
-    $(window).mousemove(function(e) {
-      svg
-      .style('left', Math.round(e.pageX - $svg.width()/2) + 'px')
-      .style('top', Math.round(e.pageY - $svg.height()/2) + 'px');
-    });
+    function setupMouse() {
+      var svg = d3.select('body')
+                .append('svg')
+                .attr('id', 'cursor-shadow')
+                .attr('height', 25)
+                .attr('width', 25)
+      var $svg = $('svg#cursor-shadow')
+      /*jshint newcap:false */
+      var circle = svg
+                   .append('circle')
+                   .attr('cx', $svg.width()/2)
+                   .attr('cy', $svg.height()/2)
+                   .attr('r', Math.min($svg.width(), $svg.height())/2 - 2)
+      $(window).mousemove(function(e) {
+        svg
+        .style('left', Math.round(e.pageX - $svg.width()/2) + 'px')
+        .style('top', Math.round(e.pageY - $svg.height()/2) + 'px');
+      });
+    }
+    
+    function setupMap() {
+      var images = [{'id':'nz-map', src:'../images/nz.jpg'}]
 
-    var cities = [
-      {name: "Auckland", loc: new LatLon(-36.840417, 174.739869), size: 30},
-      {name: "Wellington", loc: new LatLon(-41.288889, 174.777222), size: 10}
-    ]
-    
-    var north = -34
-    var west = 165.8
-    var south = -48.3
-    var east = 179.4
-    var sw = new LatLon(south, west)
-    var ne = new LatLon(north, east)
-    var height = $('img#nz-map').height()
-    var width = $('img#nz-map').width()
-    var mapping = learngeo.createMapping(ne, sw, width, height)
-    
-    d3.select('body')
-    .append('svg')
-    .attr('height', height)
-    .attr('width', width)
-    .style({position:'absolute', top: '0px', left: '0px'})
-    .append('g')
-    .selectAll('circle')
-    .data(cities)
-    .enter()
-    .append('circle')
-    .attr('class', 'city')
-    .attr('r', function(d) { return d.size + 'px' })
-    .attr('cx', function (d) { return mapping.worldToLocal(d.loc)[0] + 'px'})
-    .attr('cy', function (d) { return mapping.worldToLocal(d.loc)[1] + 'px'})
-                                                                         
+      var cities = [
+        {name: "Auckland", loc: new LatLon(-36.840417, 174.739869), size: 30},
+        {name: "Wellington", loc: new LatLon(-41.288889, 174.777222), size: 10}
+      ]
+      
+      var north = -34
+      var west = 165.8
+      var south = -48.3
+      var east = 179.4
+      var sw = new LatLon(south, west)
+      var ne = new LatLon(north, east)
+      var svg = d3.select('body')
+                .append('svg')
+                .attr({
+                  'xmlns': 'http://www.w3.org/2000/svg',
+                  'xmlns:xmlns:xlink': 'http://www.w3.org/1999/xlink'
+                })
+
+      svg.selectAll('image')
+      .data(images)
+      .enter()
+      .append('svg:image')
+      .attr('id', function(d) { return d.id })
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', $w.width() + 'px')
+      .attr('height', '1200px')
+      .attr('xlink:href', function(d) { return d.src })
+
+      var $img = $('image')
+      var mapping = learngeo.createMapping(ne, sw, $img.width(), $img.height())
+      
+
+      svg.append('g')
+      .selectAll('circle')
+      .data(cities)
+      .enter()
+      .append('circle')
+      .attr('class', 'city')
+      .attr('r', function(d) { return d.size + 'px' })
+      .attr('cx', function (d) { return mapping.worldToLocal(d.loc)[0] + 'px'})
+      .attr('cy', function (d) { return mapping.worldToLocal(d.loc)[1] + 'px'})
+    }
+
+    setupMap()
+    setupMouse()
   })
+  
 })(window,
    require('jquery'),
    require('d3'),
